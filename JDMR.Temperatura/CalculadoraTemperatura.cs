fd0c2dbb6c2bb673c6444e605dedb153b7ebe07d
Juan David Molina Ruano <juandavidmolinaruano48@gmail.com>
2024-10-11 17:43:33 -0600

    Agregar archivos de proyecto.

---
+++ JDMR.Temperatura/CalculadoraTemperatura.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JDMR.Temperatura
+{
+    public class CalculadoraTemperatura
+    {
+        public double CelsiusAFahrenheit(double celsius)
+        {
+            return (celsius * 9 / 5) + 32;
+        }
+
+        public double FahrenheitACelsius(double fahrenheit)
+        {
+            return (fahrenheit - 32) * 5 / 9;
+        }
+    }
+}
