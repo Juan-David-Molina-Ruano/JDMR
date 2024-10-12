70d5200ab1a7ddb0aa99d8bed1c9d4b5b3e7b4de
Juan David Molina Ruano <juandavidmolinaruano48@gmail.com>
2024-10-11 18:09:53 -0600

    Añadir proyecto JDMR.ManipuladorCadenas
    Se ha añadido un nuevo proyecto al archivo `JDMR.sln` llamado `JDMR.ManipuladorCadenas` con el identificador único `{65B82F88-9F1A-4808-9332-10BE02CE735F}`.
    
    Se ha creado el archivo de proyecto `JDMR.ManipuladorCadenas.csproj` con las siguientes configuraciones:
    - SDK: `Microsoft.NET.Sdk`
    - Marco de destino: `.NET 6.0`
    - `ImplicitUsings` habilitado
    - Opción `Nullable` habilitada
    
    Se ha añadido la clase `StringManipulator` en `StringManipulator.cs` con los métodos:
    - `ReverseString`: invierte una cadena de texto
    - `RemoveSpaces`: elimina los espacios de una cadena de texto
    
    Se han añadido configuraciones de compilación para `Debug` y `Release` en `JDMR.sln`.

---
+++ JDMR.ManipuladorCadenas/StringManipulator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JDMR.ManipuladorCadenas
+{
+    public class StringManipulator
+    {
+        public string ReverseString(string input)
+        {
+            //implementa la inversión de la cadena
+            return new string(input.Reverse().ToArray());
+        }
+
+        public string RemoveSpaces(string input)
+        {
+            //implementa la eliminación de espacios
+            return input.Replace(" ", "");
+        }
+    }
+}
