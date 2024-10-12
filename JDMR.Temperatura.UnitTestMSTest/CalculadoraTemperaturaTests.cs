fc4c209e3a829adca10cf211bcec82988da2547b
Juan David Molina Ruano <juandavidmolinaruano48@gmail.com>
2024-10-11 18:01:43 -0600

    Añadir pruebas unitarias para CalculadoraTemperatura
    Se han añadido nuevas pruebas unitarias en el archivo `CalculadoraTemperaturaTests.cs` utilizando MSTest.
    Se ha importado `Microsoft.VisualStudio.TestTools.UnitTesting` y `JDMR.Temperatura`.
    Se ha creado la clase `CalculadoraTemperaturaTests` con el atributo `[TestClass()]`.
    Se han añadido los métodos de prueba `CelsiusAFahrenheitTest()` y `FahrenheitACelsiusTest()`, utilizando `Assert.AreEqual` para verificar las conversiones.

---
+++ JDMR.Temperatura.UnitTestMSTest/CalculadoraTemperaturaTests.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using JDMR.Temperatura;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JDMR.Temperatura.Tests
+{
+    [TestClass()]
+    public class CalculadoraTemperaturaTests
+    {
+        [TestMethod()]
+        public void CelsiusAFahrenheitTest()
+        {
+            CalculadoraTemperatura calculadora = new CalculadoraTemperatura();
+
+            double resultado = calculadora.CelsiusAFahrenheit(40);
+
+            Assert.AreEqual(104, resultado);
+
+        }
+
+        [TestMethod()]
+        public void FahrenheitACelsiusTest()
+        {
+            CalculadoraTemperatura calculadora = new CalculadoraTemperatura();
+
+            double resultado = calculadora.FahrenheitACelsius(104);
+
+            Assert.AreEqual(40, resultado);
+        }
+    }
+}
