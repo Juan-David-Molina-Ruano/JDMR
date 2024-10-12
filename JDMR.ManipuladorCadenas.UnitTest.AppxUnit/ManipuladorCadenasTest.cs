6207a9b6ae0651a28d787804e8f9813e300a8212
Juan David Molina Ruano <juandavidmolinaruano48@gmail.com>
2024-10-11 18:21:08 -0600

    Añadir métodos de prueba en ManipuladorCadenasTest
    Se han añadido dos nuevos métodos de prueba en la clase `ManipuladorCadenasTest` dentro del archivo `ManipuladorCadenasTest.cs`:
    
    1. `ReverseStringTest`: Prueba la funcionalidad de reversión de cadenas.
    2. `RemoveSpacesTest`: Prueba la funcionalidad de eliminación de espacios en cadenas.

--- JDMR.ManipuladorCadenas.UnitTest.AppxUnit/ManipuladorCadenasTest.cs
+++ JDMR.ManipuladorCadenas.UnitTest.AppxUnit/ManipuladorCadenasTest.cs
@@ -8,5 +8,29 @@ namespace JDMR.ManipuladorCadenas.UnitTest.AppxUnit
 {
     public class ManipuladorCadenasTest
     {
+        [Fact]
+        public void ReverseStringTest()
+        {
+            StringManipulator stringManipulator = new StringManipulator();
+
+            string cadena = "Hola Mundo";
+
+            string resultado = stringManipulator.ReverseString(cadena);
+
+            Assert.Equal("odnuM aloH", resultado);
+        }
+
+        [Fact]
+        public void RemoveSpacesTest()
+        {
+            StringManipulator stringManipulator = new StringManipulator();
+
+            string cadena = "Hola mundo !";
+
+            string resultado = stringManipulator.RemoveSpaces(cadena);
+
+            Assert.Equal("Holamundo!", resultado);
+
+        }
     }
 }
